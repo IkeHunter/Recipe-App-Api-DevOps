2e5f4d9f69dbb0710f92ce5c9cad400da9d10f40
Isaac Hunter <ikehunter5@gmail.com>
2023-06-14 07:20:13 -0400

    added instance profile for bastion

--- deploy/bastion.tf
+++ deploy/bastion.tf
@@ -7,10 +7,28 @@ data "aws_ami" "amazon_linux" { # retrieving info from aws
   owners = ["amazon"]
 }
 
-resource "aws_instance" "bastion" {            # instance created
-  ami           = data.aws_ami.amazon_linux.id # data from above, gets ami id
-  instance_type = "t2.micro"
-  user_data     = file("./templates/bastion/user-data.sh")
+resource "aws_iam_role" "bastion" {
+  name               = "${local.prefix}-bastion"
+  assume_role_policy = file("./templates/bastion/instance-profile-policy.json")
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "bastion_attach_policy" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly" # attach policy to iam role above. This policy is already defined in account
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${local.prefix}-bastion-instance-profile"
+  role = aws_iam_role.bastion.name
+}
+
+resource "aws_instance" "bastion" {                   # instance created
+  ami                  = data.aws_ami.amazon_linux.id # data from above, gets ami id
+  instance_type        = "t2.micro"
+  user_data            = file("./templates/bastion/user-data.sh")
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
 
   tags = merge( # allows you to add new tag, merge with common_tags
     local.common_tags,
