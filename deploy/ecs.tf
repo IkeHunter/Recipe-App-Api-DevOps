689271dfe9f9008035355d7f1e23bd3bd1fec96d
Isaac Hunter <ikehunter5@gmail.com>
2023-06-15 08:19:57 -0400

    added ecs roles

--- deploy/ecs.tf
+++ deploy/ecs.tf
@@ -3,3 +3,31 @@ resource "aws_ecs_cluster" "main" {
 
   tags = local.common_tags
 }
+
+# next 3 resources give permissions required to start tasks
+resource "aws_iam_policy" "task_execution_role_policy" {
+  name        = "${local.prefix}-task-exec-role-policy" # creates new policy
+  path        = "/"                                     # way of organizing policies
+  description = "Allow retrieving of images and adding to logs"
+  policy      = file("./templates/ecs/task-exec-role.json") # contents of policy
+}
+
+resource "aws_iam_role" "task_execution_role" {
+  name               = "${local.prefix}-task-exec-role"
+  assume_role_policy = file("./templates/ecs/assume-role-policy.json")
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution_role" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = aws_iam_policy.task_execution_role_policy.arn # sets it to policy created above
+}
+
+# this is required to give permissions to task at runtime
+resource "aws_iam_role" "app_iam_role" {
+  name               = "${local.prefix}-api-task"
+  assume_role_policy = file("./templates/ecs/assume-role-policy.json")
+
+  tags = local.common_tags
+}
